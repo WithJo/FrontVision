cafaa95fd86ce480f05bea834855c34d255852dd
WithJo <researcherDragon@gmail.com>
2024-11-12 12:57:50 +0900

    가사 하단 padding 추가

--- src/pages/Home/CurrentSongLyrics.jsx
+++ src/pages/Home/CurrentSongLyrics.jsx
@@ -1,16 +1,23 @@
-import React from 'react';
-import { Box, Typography } from '@mui/material';
+import React from "react";
+import { Box, Typography } from "@mui/material";
 
 function CurrentSongLyrics({ currentSong }) {
     return (
-        <Box sx={{ p: 2, height: '100%', display: 'flex', flexDirection: 'column' }}>
+        <Box
+            sx={{
+                p: 2,
+                height: "100%",
+                display: "flex",
+                flexDirection: "column",
+            }}
+        >
             <Typography variant="h5" sx={{ mb: 2 }}>
                 {currentSong.title}
             </Typography>
-            
-            <Box sx={{ flexGrow: 1, overflowY: 'auto' }}>
-                <Typography variant="body1" sx={{ whiteSpace: 'pre-line' }}>
-                    {currentSong.lyrics}
+
+            <Box sx={{ flexGrow: 1, overflowY: "auto", paddingBottom: "50px" }}>
+                <Typography variant="body1" sx={{ whiteSpace: "pre-line" }}>
+                    {currentSong.lyric}
                 </Typography>
             </Box>
         </Box>
