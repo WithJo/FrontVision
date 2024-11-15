9f13cfd41e636e9498b9b326ff654384dec05d4d
박신영 <psy_chopath@naver.com>
2024-11-15 19:51:13 +0900

    로고수정

--- src/components/TopSearchBar.jsx
+++ src/components/TopSearchBar.jsx
@@ -1,56 +1,56 @@
 import { AppBar, Toolbar, Typography, TextField, Box } from "@mui/material";
 
 function TopSearchBar({ isVideoVisible, setIsVideoVisible }) {
-    const handleLogoClick = () => {
-        // 현재 비디오가 보이는 상태일 때만 토글
-        if (isVideoVisible) {
-            setIsVideoVisible(false);
-        }
-    };
+  const handleLogoClick = () => {
+    // 현재 비디오가 보이는 상태일 때만 토글
+    if (isVideoVisible) {
+      setIsVideoVisible(false);
+    }
+  };
 
-    return (
-        <AppBar position="static">
-            <Toolbar>
-                <Box 
-                    onClick={handleLogoClick}
-                    sx={{ 
-                        cursor: 'pointer',
-                        '&:hover': {
-                            opacity: 0.8
-                        }
-                    }}
-                >
-                    <Typography variant="h1">Signify</Typography>
-                </Box>
-                <TextField
-                    placeholder="노래, 앨범, 아티스트 검색"
-                    size="small"
-                    sx={{
-                        backgroundColor: "white",
-                        borderRadius: 1,
-                        marginLeft: "110px",
-                        input: {
-                            color: "black",
-                        },
-                        "& .MuiOutlinedInput-root": {
-                            "& fieldset": {
-                                borderColor: "white",
-                                borderWidth: "2px",
-                            },
-                            "&:hover fieldset": {
-                                borderColor: "lightgray",
-                                borderWidth: "2px",
-                            },
-                            "&.Mui-focused fieldset": {
-                                borderColor: "white",
-                                borderWidth: "2px",
-                            },
-                        },
-                    }}
-                />
-            </Toolbar>
-        </AppBar>
-    );
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Box
+          onClick={handleLogoClick}
+          sx={{
+            cursor: "pointer",
+            "&:hover": {
+              opacity: 0.8,
+            },
+          }}
+        >
+          <Typography variant="h1">Signify</Typography>
+        </Box>
+        <TextField
+          placeholder="노래, 앨범, 아티스트 검색"
+          size="small"
+          sx={{
+            backgroundColor: "white",
+            borderRadius: 1,
+            marginLeft: "110px",
+            input: {
+              color: "black",
+            },
+            "& .MuiOutlinedInput-root": {
+              "& fieldset": {
+                borderColor: "white",
+                borderWidth: "2px",
+              },
+              "&:hover fieldset": {
+                borderColor: "lightgray",
+                borderWidth: "2px",
+              },
+              "&.Mui-focused fieldset": {
+                borderColor: "white",
+                borderWidth: "2px",
+              },
+            },
+          }}
+        />
+      </Toolbar>
+    </AppBar>
+  );
 }
 
 export default TopSearchBar;
