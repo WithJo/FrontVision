1c49e08a8585bee960d9172ebb0eb38c363d2dc4
WithJo <researcherDragon@gmail.com>
2024-11-15 10:13:37 +0900

    api 키 감추기

--- src/apis/getSong.js
+++ src/apis/getSong.js
@@ -1,7 +1,7 @@
 //import { baseURL } from "./api";
 
 export async function getSong() {
-    const baseURL = import.meta.env.VITE_API_KEY;
+    const baseURL = import.meta.env.VITE_API_URL;
     const url = new URL(`${baseURL}/albums`);
 
     const response = await fetch(url, {
